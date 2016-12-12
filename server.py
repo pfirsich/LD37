11d82db40df707db8f11470ba820464750f44b77
Joel Schumacher <joelschum@gmail.com>
2016-12-12 10:45:36 +0100

    initial commit

---
+++ server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+#from here: https://gist.github.com/gnilchee/246474141cbe588eb9fb
+
+import sys, os, socket
+from socketserver import ThreadingMixIn
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+HOST = socket.gethostname()
+
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
+
+'''
+This sets the listening port, default port 8080
+'''
+if sys.argv[1:]:
+    PORT = int(sys.argv[1])
+else:
+    PORT = 8000
+
+'''
+This sets the working directory of the HTTPServer, defaults to directory where script is executed.
+'''
+if sys.argv[2:]:
+    os.chdir(sys.argv[2])
+    CWD = sys.argv[2]
+else:
+    CWD = os.getcwd()
+
+server = ThreadingSimpleServer(('0.0.0.0', PORT), SimpleHTTPRequestHandler)
+print("Serving HTTP traffic from", CWD, "on", HOST, "using port", PORT)
+try:
+    while 1:
+        sys.stdout.flush()
+        server.handle_request()
+except KeyboardInterrupt:
+    print("\nShutting down server per users request.")
